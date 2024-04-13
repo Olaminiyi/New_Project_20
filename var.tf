36ae6be608586258a1979ee3ac3abc4ff68da12f
Olaminiyi <olaiyaolaminiyi@gmail.com>
2024-04-13 14:46:04 +0100

    fixed error

--- var.tf
+++ var.tf
@@ -53,18 +53,9 @@ variable "cidr_ipv4" {
 variable "ami-jenkins" {
   type = string
 }
-variable "subnet_id" {
-  type = string
-}
 
 
-variable "iam_instance_profile" {
-  type        = string
-  description = "IAM role for ECR access"
-}
+
 variable "keypair" {
-  type = string
-}
-variable "security_group_id" {
   type = string
 }
